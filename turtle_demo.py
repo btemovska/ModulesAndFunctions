c9f06bd6a93f1e8215169eda824b9d5412a32e92
btemovska <btemovska@gmail.com>
2020-10-18 21:44:02 -0400

    turtle_demo

---
+++ turtle_demo.py
@@ -0,0 +1,12 @@
+from turtle import forward, right, done
+#from turtle module only import forward, right and done method
+import turtle
+#use either line 1 or either 3 not both in future
+
+forward(150)  # note this is 150 degree
+right(250)  # turn right 250 degree
+turtle.circle(75) #another way is to import all methods
+forward(150)
+
+
+done()
